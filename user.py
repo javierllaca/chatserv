c250ed25634a7135594cd3537969ea9bf1673855
Javier Llaca <javier.llaca.o@gmail.com>
2016-03-24 12:16:32 -0400

    Update server and client programs

--- user.py
+++ user.py
@@ -2,6 +2,36 @@ from Queue import Queue
 import util
 
 
+class User:
+
+    def __init__(self, username, password_sha):
+        self.username = username
+        self.password_sha = password_sha
+        self.is_connected = False
+        self.last_active = float('-inf')
+        self.blocked_ips = {}
+        self.message_queue = Queue()  # thread-safe
+
+    def login(self):
+        self.is_connected = True
+        self.register_activity()
+
+    def register_activity(self):
+        self.last_active = util.current_time()
+
+    def logout(self):
+        self.is_connected = False
+
+    def enqueue_message(self, message):
+        self.message_queue.put(message)
+
+    def dump_message_queue(self):
+        messages = []
+        while not self.message_queue.empty():
+            messages.append(self.message_queue.get())
+        return messages
+
+
 class Message:
 
     def __init__(self, message_string, from_user):
@@ -15,36 +45,3 @@ class Message:
             self.from_user.username,
             self.message_string
         )
-
-
-class User:
-
-    def __init__(self, username, password_sha):
-        self.username = username
-        self.password_sha = password_sha
-        self.socket = None
-        self.ip = None
-        self.last_active = float('-inf')
-        self.blocked_ips = {}
-        self.message_queue = Queue()  # thread-safe
-
-    def login(self, socket, ip):
-        self.socket = socket
-        self.ip = ip
-        self.last_active = util.current_time()
-
-    def logout(self):
-        self.socket = None
-
-    def enqueue_message(self, message):
-        self.message_queue.put(message)
-
-    def dump_message_queue(self):
-        messages = []
-        while not self.message_queue.empty():
-            messages.append(self.message_queue.get())
-        return messages
-
-    @property
-    def is_connected(self):
-        return self.socket is not None
